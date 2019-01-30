cb3259231cd8e59aab74489f7f1d82f79e169403
Grace Feehan <35138747+seifely@users.noreply.github.com>
2019-01-30 13:51:21 +0000

    fresh update
    Started (mostly) again from blank, as is usual. Trying to keep the simiplicity down so the code is easier to debug

--- pdpython_model/agents.py
+++ pdpython_model/agents.py
@@ -1,5 +1,10 @@
 from mesa import Agent
 
+# Stratgey Key:
+    # ANGEL - Always Cooperate
+    # DEVIL - Always Defect
+    # FP - Fixed Probabilities, multiplying the payoff matrix by the likelihood of each outcome (dependant on other)
+
 
 class PDAgent(Agent):
     """ Agent member of the iterated prisoner's dilemma model """
@@ -19,28 +24,92 @@ class PDAgent(Agent):
         self.strategy = strategy
         self.move = None
 
-        if starting_move:
-            self.move = starting_move
-        else:
-            #  self.move = self.random.choice(["C", "D"])  # random behaviours - we don't want this
-            return
 
-        def pick_strategy(self, payoff):
-            new_strategy = ""
-            # bloo bloo pick strategy based on something
-            return new_strategy
-            # then need to set self.strategy to the new strategy
+        # pull in the payoff matrix (same for all agents IF WE ASSUME ALL AGENTS HAVE EQUAL PAYOFFS)
+        self.payoffs = self.model.payoffs
 
-
-        def pick_move(self, strategy, payoff):
-            move = []
-            if strategy == None:
-                # pick_strategy(self, 1)
+        # pick a strategy - either by force, or by a decision mechanism
+        def pick_strategy(self, strategy):
+            if strategy is None:
+                # decision mechanism goes here
                 return
+
+
+        # given the payoff matrix, the strategy, and any other inputs (communication, trust perception etc.)
+        # calculate the expected utility of each move, and then pick the highest
+        def pick_move(self, strategy, payoffs):
+
+            if strategy is None or [] or 0:
+                print("I don't know what to do!")
             elif strategy == "ANGEL":
-                return move == "C"
+                return "C"
             elif strategy == "DEVIL":
-                return move == "D"
+                return "D"
+
+            elif strategy = "FP":  # this is under assumption of heterogeneity of agents
+                """ FP is designed as a strategy not based on 'trust' (though it can reflect that), but instead on 
+                the logic; 'I know they know defect is the best strategy usually, just as they know I know that'. """
+
+                ppD = 0.8  # probability of partner's defection
+                ppC = 0.2  # probability of partner's cooperation
+
+                euCD = (payoffs["C, D"]
+
+
+
+
+        # increment the agent's score - for iterated games
+        def increment_score(self, current_score):
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # if starting_move:
+        #     self.move = starting_move
+        # else:
+        #     #  self.move = self.random.choice(["C", "D"])  # random behaviours - we don't want this
+        #     return
+        #
+        # def pick_strategy(self, payoff):
+        #     new_strategy = ""
+        #     # bloo bloo pick strategy based on something
+        #     return new_strategy
+        #     # then need to set self.strategy to the new strategy
+        #
+        #
+        # def pick_move(self, strategy, payoff):
+        #     move = []
+        #     if strategy == None:
+        #         # pick_strategy(self, 1)
+        #         return
+        #     elif strategy == "ANGEL":
+        #         return move == "C"
+        #     elif strategy == "DEVIL":
+        #         return move == "D"
 
 
         # @property # I AM NOT USED TO THIS SYNTAX BUT HEY LET'S TRY IT
--- pdpython_model/model.py
+++ pdpython_model/model.py
@@ -5,37 +5,38 @@ from mesa.datacollection import DataCollector
 
 from .agents import PDAgent
 
+
 class PDGrid(Model):
     """ Initial goals for the model: Single Iteration Classic Strategy. Agents access the payoff matrix
         available to them, and then make a decision based on that. """
 
     nagents = 2
-    payoffs = {}
+    # where first column is relevant to current agent accessing matrix, and the result is when it is combined w/ partner
+    # if I pick C and they defect, I lose, whereas if they pick C and I defect, I win
+    payoffs = {("C", "C"): 3,
+              ("C", "D"): 0,
+              ("D", "C"): 5,
+              ("D", "D"): 2}
     schedule_types = {"Sequential": BaseScheduler,
                       "Random": RandomActivation,
                       "Simultaneous": SimultaneousActivation}
     spatiality = False
-
+    rounds = 1
 
     def __init__(self,
                  nagents,
                  payoffs,
+                 rounds,
                  height=1,
                  width=2,
                  schedule_type="Sequential",
                  spatialty=False):
-        # super().__init__()  # This could be wrong - look at how you've structured it in boxworld. no super init
 
         self.grid = SingleGrid(width, height, torus=True)
         self.schedule_type = schedule_type
         self.payoffs = payoffs
         self.nagents = nagents
-
-        self.spatiality = spatialty  # This isn't functioning properly right now
-        # Spatial placement of agents would basically include scaling up the world depending on how many agents you want
-        # Basically, increase agents by some fixed multiple, then scale up world to fit.
-        # e.g. 40 agents, 20 x 20 world, 50 agents, 25 x 25 world etc.
-
+        self.rounds = rounds
 
         # Create the agents
         for i in range(self.nagents):
@@ -44,21 +45,18 @@ class PDGrid(Model):
             self.grid.place_agent(agent, (x, y))  # Place it in the world
             self.schedule.add(agent)
 
-        self.datacollector = DataCollector({
-            "Cooperating Agents":
-                lambda m: len([a for a in m.schedule.agents if a.move == "C"])
-        })
+        # Collect Data
+        # on agent utilities? (or on utility per strategy?)
 
         self.running = True
-        self.datacollector.collect(self)
+        # self.datacollector.collect(self)
 
     def step(self):
         """ Take a step in the model - equates to one round of decision making"""
         self.schedule.step()
-        # collect the data needed
-        self.datacollector.collect(self)
+        # self.datacollector.collect(self)  # collect data
 
-    def run(self, n):
+    def run(self, rounds):
         """ Run the model for n epochs - for the iterated Dilemma """
-        for i in range(n):
+        for i in range(rounds):
             self.step()
