4f867de73bfc29ff6d141a4f59b03d572ed361ef
Grace Feehan <35138747+seifely@users.noreply.github.com>
2020-01-08 16:14:57 +0000

    Two more tests needed
    7x7 TFT and 7x7 Random

--- pdpython_model/agents.py
+++ pdpython_model/agents.py
@@ -145,7 +145,7 @@ class PDAgent(Agent):
                 start on q strategy """
 
         elif self.pickstrat == "RDISTRO": # Random Distribution of the two selected strategies
-            choices = ["VPP", "TFT"]
+            choices = ["VPP", "WSLS"]
             if not self.checkerboard:
                 if not self.lineplace:
                     strat = random.choice(choices)
