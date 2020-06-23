a3f779d21c16e10b21908d6b9554ef4c93c0eb26
Grace Feehan <35138747+seifely@users.noreply.github.com>
2020-06-23 18:01:30 +0100

    started adding functions

--- pdpython_model/sarsa.py
+++ pdpython_model/sarsa.py
@@ -31,10 +31,31 @@ def egreedy_action(e, qtable, current_state):
             return 'D'
 
 
-def sarsa(alpha, epsilon, gamma):
+def sarsa_decision(alpha, epsilon, gamma):
     # initialise q table
-
+    # choose action
+    # observe reward and the resulting state (S') (check partner, new state)
+    # output the next action we should take using egreedy and the next projected state
+    # after action staged, in the
     return
+
+def output_sprime(current_state, observed_action):
+    """ Returns a tuple that adds the observed action to the stack. Current state should be a tuple of strings?"""
+    cstate = list(current_state)
+    cstate.pop(0)
+    cstate.append(observed_action)
+    return tuple(cstate)
+
+def update_epsilon(initial, current, max_round, linear):
+    # Version WITHOUT simulated annealing, though that could be in V2
+    if linear:
+        increment = initial / max_round
+        new_epsilon = current - increment
+        return new_epsilon
+    else:
+        increment = current / 50        # any better value to use rather than arbitrary?
+        new_epsilon = current - increment
+        return new_epsilon
 
 
 def update_q(reward, gamma, alpha, oldQ, nextQ):
@@ -44,3 +65,6 @@ def update_q(reward, gamma, alpha, oldQ, nextQ):
 
     return newQ
 
+
+# to integrate SVO into sarsa, we could try two methods
+# a: weight the value of rewards received in each state my your orientation
